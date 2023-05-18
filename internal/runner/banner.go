b1e107c25fc4681a3105eaf1e43ef0df0c9a97cb
shubhamrasal <shubhamdharmarasal@gmail.com>
2023-05-18 12:28:09 +0530

    add prefix v to version

--- internal/runner/banner.go
+++ internal/runner/banner.go
@@ -1,10 +1,9 @@
 package runner
 
 import (
-    "github.com/projectdiscovery/gologger"
+	"github.com/projectdiscovery/gologger"
 	updateutils "github.com/projectdiscovery/utils/update"
 )
-
 
 const banner = `
             __           __              __  
@@ -15,7 +14,7 @@ const banner = `
 `
 
 // version is the current version of cdncheck
-const version = `1.0.3`
+const version = `v1.0.3`
 
 // showBanner is used to show the banner to the user
 func showBanner() {
