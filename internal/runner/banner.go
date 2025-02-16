ab9393e1bca57d24dfe5da6c54bbbe0c7eaf7d00
GitHub Action <action@github.com>
2025-02-16 10:46:50 +0000

    Bump version

--- internal/runner/banner.go
+++ internal/runner/banner.go
@@ -14,7 +14,7 @@ const banner = `
 `
 
 // version is the current version of cdncheck
-const version = `v1.1.2`
+const version = `v1.1.3`
 
 // showBanner is used to show the banner to the user
 func showBanner() {
