0d3d2054dbdb083c964d7353e3e3e81a6d7771a9
BPplays <suzuko.oss@proton.me>
2025-08-13 20:52:57 -0700

    added tests for ipv6

--- cdncheck_test.go
+++ cdncheck_test.go
@@ -10,11 +10,22 @@ import (
 func TestCDNCheckValid(t *testing.T) {
 	client := New()
 
-	found, provider, itemType, err := client.Check(net.ParseIP("173.245.48.12"))
+	found, provider, itemType, err := client.Check(net.ParseIP("2400:cb00::1"))
 	require.Equal(t, "cloudflare", provider, "could not get correct provider")
 	require.Equal(t, "waf", itemType, "could not get correct item type")
 	require.Nil(t, err, "Could not check ip in ranger")
 	require.True(t, found, "Could not check cloudlfare ip blacklist")
+
+	found, provider, itemType, err = client.Check(net.ParseIP("173.245.48.12"))
+	require.Equal(t, "cloudflare", provider, "could not get correct provider")
+	require.Equal(t, "waf", itemType, "could not get correct item type")
+	require.Nil(t, err, "Could not check ip in ranger")
+	require.True(t, found, "Could not check cloudlfare ip blacklist")
+
+
+	found, _, _, err = client.Check(net.ParseIP("::1"))
+	require.Nil(t, err, "Could not check ip in ranger")
+	require.False(t, found, "Localhost IP found in blacklist")
 
 	found, _, _, err = client.Check(net.ParseIP("127.0.0.1"))
 	require.Nil(t, err, "Could not check ip in ranger")
