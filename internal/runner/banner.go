2b3e4924f614ffc5eba92a16e2845b371fa872ef
Doğan Can Bakır <dogancanbakir@protonmail.com>
2026-09-08 11:21:23 +0300

    fix: sync banner.go version with release tag

--- internal/runner/banner.go
+++ internal/runner/banner.go
@@ -14,7 +14,7 @@ const banner = `
 `
 
 // version is the current version of cdncheck
-const version = `v1.2.52`
+const version = `v1.3.0`
 
 // showBanner is used to show the banner to the user
 func showBanner() {
